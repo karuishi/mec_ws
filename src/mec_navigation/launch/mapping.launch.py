eaa503d834b33b5ed00c24ed4f2bdc15795c6878
Vitória Matos <vitorianascimentomatos@gmail.com>
2025-09-11 16:47:45 -0300

    Add initial RViz configuration for mapping visualization
    - Created a new RViz configuration file for the mapping visualization.
    - Configured various panels including Displays, Selection, Tool Properties, and Views.
    - Set up visualization manager with multiple display types such as Grid, RobotModel, TF, Path, Camera, LaserScan, and IMU.
    - Defined global options including background color and fixed frame.
    - Configured tools for interaction, including setting initial pose and goal.
    - Enabled SlamToolboxPlugin for enhanced mapping capabilities.

--- src/mec_navigation/launch/mapping.launch.py
+++ src/mec_navigation/launch/mapping.launch.py
@@ -1,69 +1,72 @@
-import os
+""" A launch file for slam toolbox used for mapping the environment"""
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.conditions import IfCondition
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+    slam_params_file = LaunchConfiguration('slam_params_file')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    saved_map_path = LaunchConfiguration('saved_map_path')
+    use_saved_map = LaunchConfiguration('use_saved_map')
 
-    pkg_mec_navigation = get_package_share_directory('mec_navigation')
-
-    rviz_launch_arg = DeclareLaunchArgument(
-        'rviz', default_value='true',
-        description='Open RViz'
+    default_map_path = PathJoinSubstitution(
+        [FindPackageShare("mec_navigation"), 'mapas', 'finalmente2_robocup_serial']
+    )
+    
+    slam_params_file_arg = DeclareLaunchArgument(
+        'slam_params_file',
+        default_value=PathJoinSubstitution(
+            [FindPackageShare("mec_navigation"), 'config', 'slam_toolbox_mapping.yaml']
+        ),
+        description='Full path to the ROS2 parameters file to use for the slam_toolbox node',
+    )
+    
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='use simulation/Gazebo clock'
+    )
+    
+    use_saved_map_arg = DeclareLaunchArgument(
+        'use_saved_map',
+        default_value='false',
+        description='Whether to use a saved map or not'
     )
 
-    rviz_config_arg = DeclareLaunchArgument(
-        'rviz_config', default_value='mapping.rviz',
-        description='RViz config file'
+    saved_map_path_arg = DeclareLaunchArgument(
+        'saved_map_path',
+        default_value=default_map_path,
+        description='Path to the saved map'
+    )
+    
+    slam_node_saved_map = Node(
+        condition=IfCondition(use_saved_map),
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        parameters=[slam_params_file, {'use_sim_time': use_sim_time, 'map_file_name': saved_map_path}],
     )
 
-    sim_time_arg = DeclareLaunchArgument(
-        'use_sim_time', default_value='False',
-        description='Flag to enable use_sim_time'
+    slam_node_no_map = Node(
+        condition=UnlessCondition(use_saved_map),
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        parameters=[slam_params_file, {'use_sim_time': use_sim_time, 'map_file_name': ''}],
     )
 
-    # Path to the Slam Toolbox launch file
-    slam_toolbox_launch_path = os.path.join(
-        get_package_share_directory('slam_toolbox'),
-        'launch',
-        'online_async_launch.py'
-    )
+    return LaunchDescription([
+        use_sim_time_arg,
+        saved_map_path_arg,
+        use_saved_map_arg,
+        slam_params_file_arg,
+        slam_node_saved_map,
+        slam_node_no_map
+    ])
 
-    slam_toolbox_params_path = os.path.join(
-        get_package_share_directory('mec_navigation'),
-        'config',
-        'slam_toolbox_mapping.yaml'
-    )
-
-    # Launch rviz
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        arguments=['-d', PathJoinSubstitution([pkg_mec_navigation, 'rviz', LaunchConfiguration('rviz_config')])],
-        condition=IfCondition(LaunchConfiguration('rviz')),
-        parameters=[
-            {'use_sim_time': LaunchConfiguration('use_sim_time')},
-        ]
-    )
-
-    slam_toolbox_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(slam_toolbox_launch_path),
-        launch_arguments={
-                'use_sim_time': LaunchConfiguration('use_sim_time'),
-                'slam_params_file': slam_toolbox_params_path,
-        }.items()
-    )
-
-    launchDescriptionObject = LaunchDescription()
-
-    launchDescriptionObject.add_action(rviz_launch_arg)
-    launchDescriptionObject.add_action(rviz_config_arg)
-    launchDescriptionObject.add_action(sim_time_arg)
-    launchDescriptionObject.add_action(rviz_node)
-    launchDescriptionObject.add_action(slam_toolbox_launch)
-
-    return launchDescriptionObject
+if __name__ == '__main__':
+    generate_launch_description()
